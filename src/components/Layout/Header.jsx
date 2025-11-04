1453ee466fe4c168b0089a6854b94398e99c67db
outsideinsiders1-ops <outsideinsiders1@gmail.com>
2025-11-04 15:03:13 -0500

    Create Header.jsx

---
+++ src/components/Layout/Header.jsx
@@ -0,0 +1,17 @@
+// src/components/Layout/Header.jsx
+// App header component
+
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="header-content">
+        <h1>Outside Insiders</h1>
+        <p>Discover all public recreation spaces in one place</p>
+      </div>
+    </header>
+  )
+}
+
+export default Header
