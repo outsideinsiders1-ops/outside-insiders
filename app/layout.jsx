8facee4d26c94a033bc31bea1d9181ef9f21ee69
Robert Fleming <robertfleming@Roberts-MacBook-Pro.local>
2025-11-21 22:05:28 -0500

    Add favicon to prevent 404 error

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,6 +3,9 @@ import './globals.css'
 export const metadata = {
   title: 'Outside Insiders - Discover Public Recreation Spaces',
   description: 'Mobile first platform to discover public recreation spaces',
+  icons: {
+    icon: 'data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100"><text y=".9em" font-size="90">🏞️</text></svg>',
+  },
 }
 
 export default function RootLayout({ children }) {
